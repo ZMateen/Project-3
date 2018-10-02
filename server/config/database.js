17edc74d224df82539543e3b7cbbaae2deb93e21
Marcin Paszt <35789694+marcinpaszt@users.noreply.github.com>
2018-10-02 19:24:27 -0400

    server, passport

---
+++ server/config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'Isolated1'
+    },
+    'database': 'trivia_db',
+    'users_table': 'users'
+};
