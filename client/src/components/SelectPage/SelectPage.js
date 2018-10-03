f410bb068473dab8ae339a2eac0c734a88b3eb55
MGillette92 <36048864+MGillette92@users.noreply.github.com>
2018-10-03 19:29:29 -0400

    updated second page

--- client/src/components/SelectPage/SelectPage.js
+++ client/src/components/SelectPage/SelectPage.js
@@ -8,17 +8,43 @@ import CharBox from ".././CharBox";
 import StageBox from ".././StageBox";
 import ResetBtn from ".././ResetBtn";
 
+import chars from "./chars.json";
+import stages from "./stages.json";
+
 import Navbar from ".././Navbar";
 import API from "../../utils/API";
 
 class SelectPage extends Component {
 
+    state = {
+        chars,
+        stages
+      };
 
   render() {
     return (
 
       <Container>
-          
+        {this.state.chars.map(char => (
+          <CharBox
+            id={char.id}
+            key={char.id}
+            image={char.image}
+          />
+        ))}
+
+        <ResetBtn />
+
+        {this.state.stages.map(stage => (
+          <StageBox
+            id={stage.id}
+            key={stage.id}
+            image={stage.image}
+          />
+        ))}
+
+        <FightBtn />
+
       </Container>
     );
   }
