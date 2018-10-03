3ae6a91fa2f606afe5692ab7c6ebea8febad63bb
MGillette92 <36048864+MGillette92@users.noreply.github.com>
2018-10-03 19:25:44 -0400

    added components for second page

--- client/src/components/CharBox/CharBox.js
+++ client/src/components/CharBox/CharBox.js
@@ -1,1 +1,18 @@
 //character select box
+
+import React from "react";
+import "./CharBox.css";
+
+
+
+const CharBox = props => (
+    <div className="char">
+        <div className="img-container">
+            <img alt={props.name} src={props.image} />
+        </div>
+    </div>
+
+);
+
+
+export default CharBox;
