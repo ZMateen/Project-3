64a36b269d44d262af86748b68c9dbdcc7c9ad9a
Zaria LaRue Mateen <32497450+ZMateen@users.noreply.github.com>
2018-10-02 21:07:18 -0400

    Update FrontPage.js

--- client/src/components/FrontPage/FrontPage.js
+++ client/src/components/FrontPage/FrontPage.js
@@ -5,20 +5,21 @@ import Headerlogo from ".././Headerlogo";
 import Info from ".././Info";
 import Login from ".././Login";
 import Signup from ".././Signup";
-
 import Navbar from ".././Navbar";
 import API from "../../utils/API";
+import './FrontPage.css';
 
 class FrontPage extends Component {
 
 
   render() {
     return (
-
+      <div className="testing">
       <Container>
         <Headerlogo></Headerlogo>
         <Info />
       </Container>
+     </div
     );
   }
 }
