9c48bed159fd7d924c830af3707ed3eca4b047f1
MGillette92 <36048864+MGillette92@users.noreply.github.com>
2018-10-03 19:32:19 -0400

    updated second page components

--- client/src/components/StageBox/StageBox.js
+++ client/src/components/StageBox/StageBox.js
@@ -1,1 +1,18 @@
 //stage boxes to select for fight
+
+import React from "react";
+import "./StageBox.css";
+
+
+
+const StageBox = props => (
+    <div className="stage">
+        <div className="img-container">
+            <img alt={props.name} src={props.image} />
+        </div>
+    </div>
+
+);
+
+
+export default StageBox;
