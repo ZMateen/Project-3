d4adb02528b237d81f4598772ca27ba23ea2e0b4
Zaria LaRue Mateen <32497450+ZMateen@users.noreply.github.com>
2018-10-02 21:13:50 -0400

    Update Signup.js

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,1 +1,77 @@
+import React from "react";
+import './Signup.css';
 
+
+class Signup extends React.Component {
+    render() {
+      return (
+        <div id="parent">
+
+        <div className="header">
+          Fight With Us!
+        </div>
+  
+        <div className="box">
+  
+            <div className="input-group">
+  
+                <label htmlFor="name">Name</label>
+  
+                <input
+                type="text"
+                name="name"
+                className="register-input"
+                placeholder="Type Your Name..."/>
+  
+            </div>
+  
+            <div className="input-group">
+  
+              <label htmlFor="email">Email</label>
+  
+              <input
+              type="text"
+              name="email"
+              className="email-input"
+              placeholder="Email..."/>
+            
+            </div>
+  
+            <div className="input-group">
+  
+              <label htmlFor="username">Username</label>
+  
+              <input
+              type="text"
+              name="username"
+              className="login-input"
+              placeholder="Username..."/>
+            
+            </div>
+  
+            <div className="input-group">
+  
+              <label htmlFor="password">Password</label>
+  
+              <input
+              type="password"
+              name="password"
+              className="pass-input"
+              placeholder="Password..."/>
+      
+            </div>
+{/*
+            <button
+            type="button"
+            className="register-btn"
+            onClick={this
+            .submitRegister
+            .bind(this)}>Welcome to the Team!</button>
+        */}
+            </div>
+        </div>
+      );
+    }
+  }
+
+  export default Signup;
