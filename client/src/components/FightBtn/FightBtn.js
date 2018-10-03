322ac40629ddd117ff12736f084268a387b683ad
MGillette92 <36048864+MGillette92@users.noreply.github.com>
2018-10-03 19:27:21 -0400

    added component files for second page

--- client/src/components/FightBtn/FightBtn.js
+++ client/src/components/FightBtn/FightBtn.js
@@ -1,1 +1,22 @@
 //start fight button on select page and on fight page
+
+import React from "react";
+import "./FightBtn.css";
+
+{/*
+make onClick function to move to fight page with selected chars and stage
+and insert the following code into button tag
+
+onClick={function name}
+*/}
+
+const FightBtn = props => (
+
+
+<button>Fight</button>
+
+);
+
+
+
+export default FightBtn;
