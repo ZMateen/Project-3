cff363dff40a51b96f83454c069f1778f0508c9a
Zaria LaRue Mateen <32497450+ZMateen@users.noreply.github.com>
2018-10-02 21:10:31 -0400

    Update Headerlogo.js

--- client/src/components/Headerlogo/Headerlogo.js
+++ client/src/components/Headerlogo/Headerlogo.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Headerlogo.css";
 
-const Headerlogo = props => (
+const Headerlogo  = props => (
   <div className="headerlogo">
-    <img src="https://vignette.wikia.nocookie.net/logopedia/images/f/f9/Mortal_kombat_original_text_by_sidneymadmax-d3kohts.png/revision/latest?cb=20140316002705" alt="Mortal Kombat"></img>
+    <img src="../bootcamp-kombat.png" alt="Bootcamp Kombat"></img>
   </div>
 );
 
