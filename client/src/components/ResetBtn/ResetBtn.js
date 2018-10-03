ec483b61c902c76a0b4e3b4e0ca6b293321786f5
MGillette92 <36048864+MGillette92@users.noreply.github.com>
2018-10-03 19:28:18 -0400

    added components for second page

--- client/src/components/ResetBtn/ResetBtn.js
+++ client/src/components/ResetBtn/ResetBtn.js
@@ -1,1 +1,21 @@
 //resets the character select screen
+import React from "react";
+import "./ResetBtn.css";
+
+{/*
+make onClick function to reset the page
+and insert the following code into button tag
+
+onClick={function name}
+*/}
+
+const ResetBtn = props => (
+
+
+<button>Reset</button>
+
+);
+
+
+
+export default ResetBtn;
