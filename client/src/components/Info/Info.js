e0eb29158ad30f473bc9dcb82751daf59c29e346
Zaria LaRue Mateen <32497450+ZMateen@users.noreply.github.com>
2018-10-04 20:49:24 -0400

    Update Info.js

--- client/src/components/Info/Info.js
+++ client/src/components/Info/Info.js
@@ -5,6 +5,14 @@ import "./Info.css";
 const Info = props => (
     <div>
         <p>
+        In a world of coding…
+In a quaint college course in Cleveland, Ohio…
+Two classmates have been throwing petty insults at each other in an attempt to validate themselves to the rest of the class.
+Now, it has all come to a head in this moment. Finally having enough of each others poppycock-hogwash-gibberish, Marcin and Zaria call upon their comrades in a fight that will divide their class, the world, in two. Who will come out on top to take the crown of Ultimate Supreme Leader of Everything and who will be shunned into oblivion?
+Use these two powerhouses’ triva skills to your will to bend the very course of history.
+        </p>
+    
+        <p>
 In this app you will play a trivia based fighting game. You will be asked multiple choice 
 questions and will deal or recieve damage based upon correct or incorrect answers. 
 Reduce the opponents heath to 0 to win, however if your health reaches 0, you lose.
